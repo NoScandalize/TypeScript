49e3dd1370c78f3e95eb381bcf24fb66fedb60ee
Douglas Wesley <douglas.almeida55@yahoo.com.br>
2023-11-22 17:48:31 -0300

    React
    - Seção 11: React com TypeScript

---
+++ 11_react_com_ts/src/components/FirstComponent.tsx
@@ -0,0 +1,10 @@
+// 3 - componente
+import React, { ReactElement } from 'react'
+
+const FirstComponent = (): ReactElement => {
+  return (
+    <div>Meu primeiro componente</div>
+  )
+}
+
+export default FirstComponent;
